c3e143eaac6e046dc77976696ea3df733573426a
GUstavo Calixto <calixto14@live.com>
2022-06-26 12:54:32 -0300

    criando estrtura de seviços

---
+++ main/Services/FirstService.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace main.Services;
+
+class FirstService{
+    public void first(){
+        Console.WriteLine("Implemente toda a logica e serviços necessario ao ETL dentro dessa estrtura");
+    }
+}
