bba65d2e7b5652d7c275c4427a5968a1c213e6b0
Bruno Viotto <brunoviottosantos@gmail.com>
2022-06-30 14:37:12 -0300

    NewTable

--- main/DataLoad/LoadContext.cs
+++ main/DataLoad/LoadContext.cs
@@ -7,7 +7,33 @@ namespace main.DataLoad;
 class LoadContext {
 
     public void conn(){
+        SalarioDoencaIdade();
         Console.WriteLine("implemente o load context e seus metodos de cargas");
     }
 
+    public void SalarioDoencaIdade()
+    {
+        using var context = new ets_dadosContext();
+
+        var source = DataSource.SourceContext.SalarioDoencaIdade();
+
+        var table = new List<NewTable>();
+
+        var group = source.GroupBy(di => di.Doenca);
+
+        foreach (var row in group)
+        {
+            table.Add(new NewTable
+            {
+                MediaSalarial = (int)row.Average(r => r.MediaSalarial),
+                Doenca = row.Key,
+                MediaIdade = (int)row.Average(r => r.MediaIdade)
+            });
+        }
+
+        foreach (var r in table)
+            Console.WriteLine($"{r.Doenca}, {r.MediaIdade}, {r.MediaSalarial}");
+
+        Console.WriteLine("\n\n");
+    }
 }
--- main/DataSource/SourceContext.cs
+++ main/DataSource/SourceContext.cs
@@ -9,4 +9,31 @@ class SourceContext{
     public void conn(){
         Console.WriteLine("Implemente a Source context e seus metodos de busca");
     }
+
+    // NewTable
+    public static List<DataLoad.NewTable> SalarioDoencaIdade()
+    {
+        using var context = new analytic_dataContext();
+
+        var dados = context.Diagnosticos
+        .Join(context.Doencas, di => di.IdDoenca, d => d.Id, (di, d) => new
+        {
+            IdPaciente = di.IdPaciente,
+            Doenca = d.Nome
+        })
+        .Join(context.Pacientes, di => di.IdPaciente, p => p.Id, (di, p) => new
+        {
+            Idade = p.Idade,
+            IdClasseSocial = p.IdClasseSocial,
+            Doenca = di.Doenca
+        })
+        .Join(context.ClasseSocials, di => di.IdClasseSocial, c => c.Id, (di, c) => new DataLoad.NewTable
+        {
+            MediaIdade = di.Idade,
+            MediaSalarial = (int)(c.SalarioPiso + c.SalarioTeto) / 2,
+            Doenca = di.Doenca
+        }).ToList();
+
+        return dados;
+    }
 }
